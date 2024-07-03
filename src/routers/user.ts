c9a234dc579c048d69b43cfb42dbaac519752651
LeeDat03 <leedat104517@gmail.com>
2024-07-03 23:32:33 +0700

    add getAllUser

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,9 +1,14 @@
 import express from "express";
 
-import { getUserById, getUserStats } from "../controllers/user-controller";
+import {
+  getAllUsers,
+  getUserById,
+  getUserStats,
+} from "../controllers/user-controller";
 
 export const user = (router: express.Router) => {
   router.route("/users/stats").post(getUserStats);
 
+  router.route("/users").get(getAllUsers);
   router.route("/users/:id").get(getUserById);
 };
