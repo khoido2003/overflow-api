c3b7a51609810522cbd0d8546ab25cc99e0d88e7
LeeDat03 <leedat104517@gmail.com>
2024-07-13 01:34:54 +0700

    add edit answer

--- src/routers/answer.ts
+++ src/routers/answer.ts
@@ -2,13 +2,22 @@ import { protect } from "../controllers/auth-controller";
 import {
   GetAllAnswers,
   createAnswerQuestion,
+  deleteAnswerById,
   downvoteAnswer,
+  editAnswer,
+  getAnswerById,
   upvoteAnswer,
 } from "../controllers/answer-controller";
 import express from "express";
 
 export const answer = (router: express.Router) => {
   router.route("/answers").post(protect, createAnswerQuestion);
+
+  router
+    .route("/answers/:id")
+    .get(getAnswerById)
+    .patch(protect, editAnswer)
+    .delete(protect, deleteAnswerById);
 
   router.route("/answers/upvotes").post(protect, upvoteAnswer);
 
